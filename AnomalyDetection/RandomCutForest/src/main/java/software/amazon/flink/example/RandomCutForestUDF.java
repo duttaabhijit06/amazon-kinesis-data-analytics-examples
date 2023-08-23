26b8c91a9fdefe860f451e8dfe1ab28a28287477
duttaabhijit06 <dutta.abhijit06@gmail.com>
2023-08-23 00:59:46 -0400

    Update RandomCutForestUDF.java

--- AnomalyDetection/RandomCutForest/src/main/java/software/amazon/flink/example/RandomCutForestUDF.java
+++ AnomalyDetection/RandomCutForest/src/main/java/software/amazon/flink/example/RandomCutForestUDF.java
@@ -5,14 +5,14 @@ import com.amazon.randomcutforest.RandomCutForest;
 
 public class RandomCutForestUDF extends ScalarFunction{
     
-    public double eval(String todo, float value) throws Exception{
-        
-        double retVal;
-        switch(todo) {
-            case "random_cut_forest": retVal = calculateAnomalyScore(value); break;
-            default: retVal = 0;
+    public double eval(float value) throws Exception{        
+        double retVal = Double.parseDouble("0");
+        retVal = calculateAnomalyScore(value); 
+        if (retVal>0){
+            return retVal;
+        }else{
+            return Double.parseDouble("0");
         }
-        return retVal;
     }
     public static void main (String[] args) throws java.lang.Exception
 	{   
